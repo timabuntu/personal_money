12735aa84176e7ccdff6651475c9820383135fbe
thiago lopes de mello <timabuntu@gmail.com>
2022-03-11 14:42:59 -0300

    feat: created dashboard styled mobile

--- src/components/TransactionsTable/index.tsx
+++ src/components/TransactionsTable/index.tsx
@@ -1,44 +1,66 @@
 const TransactionsTable: React.FC = () => {
   return (
-    <div>
-      <table className="w-full table-auto">
-        <thead>
-          <tr className="py-4 text-gray-500">
-            <th className="px-4 py-4 font-normal text-left">Título</th>
-            <th className="py-4 font-normal text-left">Valor</th>
-            <th className="py-4 font-normal text-left">Categoria</th>
-            <th className="py-4 font-normal text-left">Data</th>
-          </tr>
-        </thead>
+    <section>
+      <header className="grid grid-cols-4 px-4 py-3 text-sm text-gray-400 ">
+        <div>
+          <span>Titulo</span>
+        </div>
+        <div>
+          <span>Preço</span>
+        </div>
+        <div>
+          <span>Categoria</span>
+        </div>
+        <div>
+          <span>Data</span>
+        </div>
+      </header>
 
-        <tbody className="text-sm">
-          <tr className="h-10 bg-slate-50">
-            <td className="px-4">Criação de Post</td>
-            <td className="">R$ 70,00</td>
-            <td className="">Arte</td>
-            <td className="">10/07/1983</td>
-          </tr>
-          <tr className="h-10 bg-slate-50">
-            <td className="px-4">Criação de Post</td>
-            <td>R$ 70,00</td>
-            <td>Arte</td>
-            <td>10/07/1983</td>
-          </tr>
-          <tr className="h-10 bg-slate-50">
-            <td className="px-4">Criação de Post</td>
-            <td>R$ 70,00</td>
-            <td>Arte</td>
-            <td>10/07/1983</td>
-          </tr>
-          <tr className="h-10 bg-slate-50">
-            <td className="px-4">Criação de Post</td>
-            <td>R$ 70,00</td>
-            <td>Arte</td>
-            <td>10/07/1983</td>
-          </tr>
-        </tbody>
-      </table>
-    </div>
+      <div className="grid grid-cols-4 p-4 mt-2 text-sm rounded-md bg-slate-50">
+        <div>
+          <span className="text-gray-700">Salário</span>
+        </div>
+        <div>
+          <span className="text-green-500">R$ 4.000,00</span>
+        </div>
+        <div>
+          <span className="text-gray-400">Pagamento</span>
+        </div>
+        <div>
+          <span className="text-gray-400">07/03/2022</span>
+        </div>
+      </div>
+
+      <div className="grid grid-cols-4 p-4 mt-2 text-sm rounded-md bg-slate-50">
+        <div>
+          <span className="text-gray-700">Aluguel</span>
+        </div>
+        <div>
+          <span className="text-red-500">R$ 1.200,00</span>
+        </div>
+        <div>
+          <span className="text-gray-400">Moradia</span>
+        </div>
+        <div>
+          <span className="text-gray-400">10/03/2022</span>
+        </div>
+      </div>
+
+      <div className="grid grid-cols-4 p-4 mt-2 text-sm rounded-md bg-slate-50">
+        <div>
+          <span className="text-gray-700">Pizza</span>
+        </div>
+        <div>
+          <span className="text-red-500">R$ 70,00</span>
+        </div>
+        <div>
+          <span className="text-gray-400">Gordice</span>
+        </div>
+        <div>
+          <span className="text-gray-400">11/03/2022</span>
+        </div>
+      </div>
+    </section>
   );
 };
 
