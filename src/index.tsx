68b9c925b264cadbfc770c60888a1534910e7009
thiago lopes de mello <timabuntu@gmail.com>
2022-03-12 15:53:28 -0300

    lib: miragejs configured

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,27 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 
+import { createServer } from "miragejs";
+
+createServer({
+  routes() {
+    this.namespace = "api";
+
+    this.get("/transactions", () => {
+      return [
+        {
+          id: 1,
+          title: "Pizza",
+          amount: 70,
+          type: "withdraw",
+          category: "Gordice",
+          createdAt: new Date(),
+        },
+      ];
+    });
+  },
+});
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
