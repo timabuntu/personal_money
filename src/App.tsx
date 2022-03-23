b09bbf120a5fa72702f5b9d48f0cf81050591112
thiago lopes de mello <timabuntu@gmail.com>
2022-03-23 11:09:10 -0300

    fix: adjust button type modal

--- src/App.tsx
+++ src/App.tsx
@@ -84,34 +84,38 @@ const App: React.FC = () => {
           <input
             type="text"
             placeholder="Título"
-            className="px-4 py-4 text-gray-500 bg-gray-200 rounded-lg outline outline-1 outline-gray-300 focus:outline-none focus:ring-1 focus:ring-gray-400 placeholder-slate-400"
+            className="px-4 py-4 text-sm text-gray-500 bg-gray-200 rounded-lg outline outline-1 outline-gray-300 focus:outline-none focus:ring-1 focus:ring-gray-400 placeholder-slate-400"
           />
+
           <input
             type="text"
             placeholder="Valor"
-            className="px-4 py-4 mt-4 text-gray-500 bg-gray-200 rounded-lg outline outline-1 outline-gray-300 focus:outline-none focus:ring-1 focus:ring-gray-400 placeholder-slate-400g"
+            className="px-4 py-4 mt-4 text-sm text-gray-500 bg-gray-200 rounded-lg outline outline-1 outline-gray-300 focus:outline-none focus:ring-1 focus:ring-gray-400 placeholder-slate-400g"
           />
+
           <div className="flex justify-between gap-4 mt-4">
-            <button
-              type="button"
-              className="flex items-center justify-center flex-1 gap-8 px-4 py-4 bg-white rounded-lg outline outline-1 outline-gray-300 focus:outline-none focus:ring-1 focus:ring-gray-400"
-            >
-              <img src={incomeImg} alt="" className="" />
-              Entrada
-            </button>
-            <button className="flex items-center justify-center flex-1 gap-8 px-4 py-4 bg-white rounded-lg outline outline-1 outline-gray-300 focus:outline-none focus:ring-1 focus:ring-gray-400">
-              <img src={outcomeImg} alt="" className="" />
-              Saída
-            </button>
+            <div className="flex justify-center flex-1 gap-4 px-4 py-4 bg-white rounded-lg cursor-pointer outline outline-1 outline-gray-300 focus:outline-none focus:ring-1 focus:ring-gray-400">
+              <img src={incomeImg} alt="Entrada" className="w-6 h-6" />
+              <button type="button" className="text-sm">
+                Entrada
+              </button>
+            </div>
+
+            <div className="flex justify-center flex-1 gap-4 px-4 py-4 bg-white rounded-lg cursor-pointer outline outline-1 outline-gray-300 focus:outline-none focus:ring-1 focus:ring-gray-400">
+              <img src={outcomeImg} alt="Saída" className="w-6 h-6" />
+              <button className="text-sm ">Saída</button>
+            </div>
           </div>
+
           <input
             type="text"
             placeholder="Categoria"
-            className="px-4 py-4 mt-4 text-gray-500 bg-gray-200 rounded-lg outline outline-1 outline-gray-300 focus:outline-none focus:ring-1 focus:ring-gray-400 placeholder-slate-400"
+            className="px-4 py-4 mt-4 text-sm text-gray-500 bg-gray-200 rounded-lg outline outline-1 outline-gray-300 focus:outline-none focus:ring-1 focus:ring-gray-400 placeholder-slate-400"
           />
+
           <button
             type="submit"
-            className="py-4 mt-4 font-semibold transition rounded-lg text-slate-50 bg-emerald-500 hover:bg-emerald-600"
+            className="py-4 mt-4 text-sm font-semibold transition rounded-lg text-slate-50 bg-emerald-500 hover:bg-emerald-600"
           >
             Cadastrar
           </button>
